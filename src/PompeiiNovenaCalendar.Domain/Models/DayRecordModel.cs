b928ef55c586cc433ed2ef984d9d5a1ca8a7f173
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2025-03-09 16:09:31 +0100

    Added basic view layouts and fixes small issues

--- src/PompeiiNovenaCalendar.Domain/Models/DayRecordModel.cs
+++ src/PompeiiNovenaCalendar.Domain/Models/DayRecordModel.cs
@@ -1,12 +1,24 @@
 ﻿namespace PompeiiNovenaCalendar.Domain.Models
 {
-    public class DayRecordModel(int Id, DateTime Date)
+    public class DayRecordModel
     {
+        public DayRecordModel(int id, DateTime day)
+        {
+            Id = id;
+            Day = day;
+        }
+
+        public int Id { get; set; }
+
+        public DateTime Day { get; set; }
+
+        public string StatusName => IsCompleted ? "Wykonany" : "Niewykonany"; //change it to icon
+
         public bool[] MysteriesStatus { get; private set; } = new bool[4];
 
         public bool IsCompleted => MysteriesStatus.Take(3).Count(status => status) >= 3;
 
-        public bool CanEdit() => Date <= DateTime.Today;
+        public bool CanEdit() => Day <= DateTime.Today;
 
         public void SetMysteryStatus(int index, bool value)
         {
--- src/PompeiiNovenaCalendar/AppShell.xaml.cs
+++ src/PompeiiNovenaCalendar/AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace PompeiiNovenaCalendar
+﻿using PompeiiNovenaCalendar.Presentation.Views;
+
+namespace PompeiiNovenaCalendar
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(DaysListPage), typeof(DaysListPage));
         }
     }
 }
